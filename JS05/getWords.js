2c88cbc6b3c1648d3c5609b7e401398360a5ac70
adamoutankari@gmail.com <adamoutankari@gmail.com>
2023-02-16 09:09:40 +0100

    Adamou1/JS02

---
+++ JS05/getWords.js
@@ -0,0 +1,11 @@
+function getWords(){
+  let arr= test.split(' ');
+  const test= arr
+  if(arr.length<2){
+    return arr[1];
+
+https://github.com/Adamou1/Adamou1-JS02.git
+  }
+  console.log(test(["Hello",[26,"codeloccol",["sadire","JavaScript","Cs"]],42,"Hackathon"]));
+}
+getWords(); 
