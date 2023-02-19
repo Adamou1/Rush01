5a953db6f7cb90a74292376beebf467c0959037c
Adamou1 <103080158+Adamou1@users.noreply.github.com>
2023-02-19 23:53:39 +0100

    Delete Js04 directory

--- Js04/countOneToHunds.js
+++
@@ -1,8 +0,0 @@
-function countOneToHunds() {
-for(var i = 1; i <= 100; i++) {
-console.log(i);
- }
-}
-
-countOneToHunds();
-module.exports= countOneToHunds;
--- Js04/multiply.js
+++
@@ -1,5 +0,0 @@
-function multiply(a=5, b=10){
-  return a*b;
-}
-console.log(multiply(a=5, b=10));
-module.exports= multiply;
