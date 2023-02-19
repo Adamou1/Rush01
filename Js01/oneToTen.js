6154af3ebac180b27d3db495ac86124bcea96a12
Adamou1 <103080158+Adamou1@users.noreply.github.com>
2023-02-19 23:53:01 +0100

    Delete Js01 directory

--- Js01/oneToTen.js
+++
@@ -1,5 +0,0 @@
-var sum=0;
-for (var i=1; i<=10; i++) {
-  sum+=i;
-}
-console.log('the sum of 1 to 10 is:',sum);
--- Js01/weekendsVerified.js
+++
@@ -1,7 +0,0 @@
-let jour='lundi';
-if (jour='') {
-  samedi=='weekend' ;
-} else{
-  samedi=='weekday';
-}
-console.log(samedi);
